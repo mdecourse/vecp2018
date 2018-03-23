17b331ada5f411f5be852ffc9217fec919e6ca82
機械設計工程系課程 (NFU-MDE-Taiwan) <mdecourse@gmail.com>
2018-03-23 21:30:52 +0800

    Create guess_number1.lua

---
+++ lua/SchoolProject/examples/guess_number1.lua
@@ -0,0 +1,24 @@
+﻿-- 導入 js 模組
+js = require("js")
+-- 取得 window
+window = js.global
+-- 猜小於或等於 n 的整數
+big = 10
+-- 計算猜測次數, 配合 while 至少會猜一次
+num = 1
+-- 利用 window:prompt 方法回應取得使用者所猜的整數
+guess = window:prompt("請猜一個介於 1 到 "..big.." 的整數")
+-- 利用數學模組的 random 函數以亂數產生答案
+answer = math.random(big)
+
+-- 若沒猜對, 一直猜到對為止
+while answer ~= tonumber(guess) do
+    num = num + 1
+    if answer > tonumber(guess) then
+        guess = window:prompt("猜第 "..num.." 次, guess="..guess..", answer="..answer.." - too small")
+    end
+    if answer < tonumber(guess) then
+        guess = window:prompt("猜第 "..num.." 次, guess="..guess..", answer="..answer.." - too big")
+    end 
+end
+print("總共猜了 "..num.." 次, answer=guess="..answer.." - correct")

cbbafc4d0de4ae22336fed1049146daa7423df57
scrum-1 <scrum1@mde.tw>
2018-04-04 01:17:45 +0800

    modify cango1

--- lua/SchoolProject/ext1.lua
+++ lua/SchoolProject/ext1.lua
@@ -207,7 +207,11 @@ canvas:setWorldCoordsRHC(0, 0, 80)
 canvas:drawText("gc.setWorldCoordsRHC() 設為 Y 向上為正", Object{x=2, y=52, fontSize=23, fillColor="black"})
 -- Lua 的 Table 轉為 Javascript Array (在 Python 為數列) 的方法
 t = {"M",0,0, "L", 35*math.cos(30*deg), 35*math.sin(30*deg), 0, 0}
-t_js_array = js.global:Array(table.unpack(t))
+-- one way to convert t table to javascript array
+--t_js_array = js.global:Array(table.unpack(t))
+-- better way
+t_js_array = js.global:Array()
+t_js_array:splice(0, 0, table.unpack(t))
 canvas:drawPath(t_js_array)
     ]]
 end
